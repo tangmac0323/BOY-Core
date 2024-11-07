a74306c812a1eb67738b5ef4b68d4a1d0980cd57
tangmac0323 <tangmac0323@gmail.com>
2024-11-06 20:01:44 -0800

    fix

--- api/writesetup.js
+++ api/writesetup.js
@@ -14,11 +14,13 @@ const writesetup = async (request, response) => {
       if (!hashBuffer || !encryptedFormValues)
         throw new Error('hashBuffer and encryptedFormValues required');
 
+      console.log('writesetup -sending to postgres api')
       await sql`INSERT INTO hashed_formation_setup (hashed_setup_code, encrypted_setup_code) 
             VALUES (${hashBuffer}, ${encryptedFormValues}) 
             ON CONFLICT (hashed_setup_code) DO UPDATE
             SET encrypted_setup_code=${encryptedFormValues}`;
     } else {
+      console.log('writesetup - method not allowed')
       response.status(405).end(); // Method Not Allowed
     }
   } catch (error) {
