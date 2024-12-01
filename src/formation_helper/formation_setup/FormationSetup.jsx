bbfd3c39658d8aec13456c042b6f9750c326635c
tangmac0323 <tangmac0323@gmail.com>
2024-12-01 00:19:03 -0800

    miss this one

--- src/formation_helper/formation_setup/FormationSetup.jsx
+++ src/formation_helper/formation_setup/FormationSetup.jsx
@@ -16,6 +16,7 @@ const FormationSetup = () => {
       <TeamBlock teamNumber="0" />
       <TeamBlock teamNumber="1" />
       <TeamBlock teamNumber="2" />
+      <TeamBlock teamNumber="3" />
     </div>
   );
 };
